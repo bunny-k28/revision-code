0fc236e00fbc229eb5223c2469a873433f03aa46
bunny-k28 <58970641+bunny-k28@users.noreply.github.com>
2021-02-16 11:33:29 +0530

    Create looping.py

---
+++ looping.py
@@ -0,0 +1,16 @@
+# while loop
+x = 1  # counter or incrementing value
+while x<=5:
+     print('hello')
+     # break  # statement to end the any loop
+     x = x + 2  # incrementation value
+print('loop ended')
+
+# for loop
+# case 1
+for i in list2:
+     print(i, end='')  # end key word to print horizontaly
+
+# for loop case 2
+for i in range( 0, 5):
+     print(i)
